f26c1b351ca234bc5f48c7a22e6c6ad771debf2f
2D31Elinda <115599039+2D31Elinda@users.noreply.github.com>
2022-10-12 09:15:43 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,44 @@
+let counter = 0
+input.onButtonPressed(Button.A, function () {
+    counter = 10
+    for (let index = 0; index < 10; index++) {
+        counter += -1
+        basic.showNumber(counter)
+    }
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+})
